ffe908b4dae71431a7528a9ba64ab96917355213
JDenman6 <jdenman6@gmail.com>
2017-04-08 09:55:13 -0400

    update static_page titles

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,19 +1,26 @@
 require 'test_helper'
 
+def setup
+  
+end
+
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | SaberNSavor"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "Help | SaberNSavor"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
+    assert_select "title", "About | SaberNSavor"
   end
 
 end
