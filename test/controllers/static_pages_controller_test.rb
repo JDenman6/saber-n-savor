ada80c82937843f8e5b1d3b90ecb476f3ead898f
JDenman6 <jdenman6@gmail.com>
2017-04-08 10:00:23 -0400

    finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,10 +1,15 @@
 require 'test_helper'
 
 def setup
-  
+
 end
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
+  
   test "should get home" do
     get static_pages_home_url
     assert_response :success
