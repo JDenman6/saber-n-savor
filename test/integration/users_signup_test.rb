14f51e93d7de6600a418c52efd848297a76e2f85
JDenman6 <jdenman6@gmail.com>
2017-04-12 20:41:33 -0400

    handle successful user creation, add flash messages to application layout, and test for successful user creation

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -17,4 +17,17 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_template 'users/new'
     assert_select 'div.field_with_errors'
   end
+
+  test "valid signup information" do
+    get signup_path
+
+    assert_difference 'User.count', 1 do
+      post users_path, params: {user: { name: "Valid Name",
+                                        email: "valid@email.com",
+                                        password: "validPassword",
+                                        password_confirmation: "validPassword" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+  end
 end
