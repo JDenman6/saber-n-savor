aca37b42b4e68cb1b0d94740eedf5b5c3d4264d4
JDenman6 <jdenman6@gmail.com>
2017-04-18 17:46:47 -0400

    add persistent session logout functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,7 +23,7 @@ class User < ApplicationRecord
     SecureRandom.urlsafe_base64
   end
 
-  # Remembers a user in the database for use in persistant sessions.
+  # Remembers a user in the database for use in persistent sessions.
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
@@ -32,5 +32,10 @@ class User < ApplicationRecord
   # Checks the authenticity of a given remember_token. returns boolean.
   def authentic?(remember_token)
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # Delete the user's current session token.
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
