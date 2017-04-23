7924e6f61a3297e6f60d3a98aad577898aae0bd8
JDenman6 <jdenman6@gmail.com>
2017-04-23 09:56:36 -0400

    add example fixtures

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,14 +4,14 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   def setup
     @user       = users(:one)
-    @other_user = users(:three)
+    @other_user = users(:two)
   end
 
   test "should redirect index when not logged in" do
     get users_path
     assert_redirected_to login_url
   end
-  
+
   test "should get new" do
     get signup_path
     assert_response :success
