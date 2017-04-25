fae5614cd9b89a78a1f8c8dff83fb8f7490609b1
JDenman6 <jdenman6@gmail.com>
2017-04-25 18:57:48 -0400

    protect Users#show and Users#index from inactive users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,11 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    (redirect_to root_path and return) unless @user.activated?
   end
 
   def index
-    @users = User.paginate(page: params[:page], per_page: 10)
+    @users = User.where(activated: true).paginate(page: params[:page], per_page: 10)
   end
 
   def new
