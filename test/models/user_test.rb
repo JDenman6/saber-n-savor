b55376fa27502a8fe4533d4c4689121484a4e058
JDenman6 <jdenman6@gmail.com>
2017-05-04 09:00:40 -0400

    add test for mp's destroyed when user destroyed

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -75,4 +75,12 @@ class UserTest < ActiveSupport::TestCase
   test "user.authentic? should return false if remember_digest.nil?" do
     assert_not @user.authentic?('')
   end
+
+  test "associated microposts should be destroyed" do
+    @user.save
+    @user.microposts.create!(content: "Lorem ipsum")
+    assert_difference 'Micropost.count', -1 do
+      @user.destroy
+    end
+  end
 end
