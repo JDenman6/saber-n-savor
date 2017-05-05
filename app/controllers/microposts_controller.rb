a5068db77476828f537856849d4653ab366226a8
JDenman6 <jdenman6@gmail.com>
2017-05-05 18:40:37 -0400

    add Microposts#create action

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -2,6 +2,13 @@ class MicropostsController < ApplicationController
   before_action :log_in_user, only: [:create, :destroy]
 
   def create
+    @micropost = current_user.microposts.build(micropost_params)
+    if @micropost.save
+      flash[:success] = "Micropost created!"
+      redirect_to root_url
+    else
+      render 'static_pages/home'
+    end
   end
 
   def destroy
