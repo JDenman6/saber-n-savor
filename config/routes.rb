74803c3ee3131facfcdcf447f546fce5921670a2
JDenman6 <jdenman6@gmail.com>
2017-05-10 19:00:24 -0400

    add route to fix minor bug
    If a user were to upload an image that is too big, even after being warned by client-side validations,
    and they hit post at the root url, they would be brought to /microposts. If they then refresh their browser,
    it would put out a get request with no valid route. I fixed this by specifying the the route for
    get microposts to StaticPages#home.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   get 'password_resets/new'
-
   get 'password_resets/edit'
 
   root 'static_pages#home'
@@ -15,5 +14,6 @@ Rails.application.routes.draw do
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
+  get    '/microposts', to: 'static_pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
