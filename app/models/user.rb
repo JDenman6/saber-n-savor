5880f79e4832492594144fd4d3e529a577e0e63f
JDenman6 <jdenman6@gmail.com>
2017-05-16 13:12:11 -0400

    add passive_relationships (followers) to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,16 @@ class User < ApplicationRecord
                                   foreign_key: "follower_id",
                                   dependent:   :destroy
   has_many :following, through: :active_relationships, source: :followed
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+  has_many :followers, through: :passive_relationships, source: :follower
+
   attr_accessor :remember_token, :activation_token, :reset_token
+
   before_save :downcase_email
   before_create :create_activation_digest
+
 
   validates :name, presence: true, length: {maximum: 51}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
