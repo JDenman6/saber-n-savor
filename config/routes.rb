94ce87f7ac88a098b4314bf44379be277e934bf3
JDenman6 <jdenman6@gmail.com>
2017-05-18 08:49:57 -0400

    add relationships#create and #destroy to routes
    and straighten up code

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
 
-  get  '/help', to: 'static_pages#help'
-  get  '/about', to: 'static_pages#about'
-  get  '/signup',  to: 'users#new'
-  post '/signup',  to: 'users#create'
-  get  '/login',   to: 'sessions#new'
-  post '/login',  to: 'sessions#create'
+  get  '/help',     to: 'static_pages#help'
+  get  '/about',    to: 'static_pages#about'
+  get  '/signup',   to: 'users#new'
+  post '/signup',   to: 'users#create'
+  get  '/login',    to: 'sessions#new'
+  post '/login',    to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   resources :users do
     member do
@@ -16,6 +16,6 @@ Rails.application.routes.draw do
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
-  get    '/microposts', to: 'static_pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get    '/microposts',             to: 'static_pages#home'
+  resources :relationships,       only: [:create, :destroy]
 end
