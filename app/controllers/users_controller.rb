037f4001ddf7b992322d02bb345e4e3d58b7a700
JDenman6 <jdenman6@gmail.com>
2017-05-20 17:31:52 -0400

    Fixup! fix typos in Users#following and #followers
    added missing paginate code in each and included leading underscore in render line for each

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,15 +49,15 @@ class UsersController < ApplicationController
   def following
     @title = "Following"
     @user  = User.find(params[:id])
-    @users = @user.following
-    render 'show_follow'
+    @users = @user.following.paginate(page: params[:page])
+    render '_show_follow'
   end
 
   def followers
     @title = "Followers"
     @user  = User.find(params[:id])
-    @users = @user.followers 
-    render 'show_follow'
+    @users = @user.followers.paginate(page: params[:page])
+    render '_show_follow'
   end
 
   private
