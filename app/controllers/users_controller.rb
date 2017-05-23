d3723a140e43454bc1656c85b04b8090e60090a9
JDenman6 <jdenman6@gmail.com>
2017-05-22 20:57:47 -0400

    add #following and #followers to log_in_user action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action :log_in_user,    only: [:index, :edit, :update, :destroy]
+  before_action :log_in_user,    only: [:index, :edit, :update, :destroy,
+                                        :following, :followers]
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_only,     only: [:destroy]
 
