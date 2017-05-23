679fea790a1a80b817904c501cb6fc9c992eea28
JDenman6 <jdenman6@gmail.com>
2017-05-22 21:11:13 -0400

    Fixup! fix user controller test
    both #following and #followers should redirect to login_url
    (rather than root_url)

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -80,11 +80,11 @@ end
 
   test "should redirect following when not logged in" do
     get following_user_path @user
-    assert_redirected_to root_url
+    assert_redirected_to login_url
   end
 
   test "should redirect followers when not logged in" do
     get followers_user_path @user
-    assert_redirected_to root_url
+    assert_redirected_to login_url
   end
 end
