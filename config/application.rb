22192982d7a3a0823f523ede3e558a31a69eb51d
JDenman6 <jdenman6@gmail.com>
2017-05-25 08:54:43 -0400

    add js versions of follow/unfollow

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,7 @@ module SaberNSavor
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.serve_static_assets = true
+    # Include authenticity tokens in remote forms
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
